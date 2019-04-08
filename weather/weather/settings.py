7b5422de4bbbe2b10ce31c466121fae30518ab56
USER_NAME <EMAIL_ADDRESS>
2019-04-08 19:30:18 +0000

    added more to settings.py

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/assets/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'assets'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'assets'),
+#]
 
