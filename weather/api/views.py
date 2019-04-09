6e0ed0f70deef8fb99da5818bd00df42d69149ba
USER_NAME <EMAIL_ADDRESS>
2019-04-09 19:10:19 +0000

    changed views.py

--- weather/api/views.py
+++ weather/api/views.py
@@ -3,13 +3,10 @@ from rest_framework import generics
 from .models import Temperature
 from .serializers import TemperatureSerializer
 
-
-
-class TemperatureList(generics.ListAPIView):
+class TemperatureList(generics.ListCreateAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
     
 class TemperatureDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
-    
