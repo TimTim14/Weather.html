76b52784cd70a801b5ab3cd728a31f0c5cabb5a5
USER_NAME <EMAIL_ADDRESS>
2019-04-15 19:07:32 +0000

    made the website post

--- weather/api/models.py
+++ weather/api/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
-
 class Temperature(models.Model):
     celsius = models.FloatField(default=0.0)
-                                       
     change = models.FloatField(default=0.0)
     recorded_time = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return str(self.celsius)
-    
-    
-class Humidity(models.Model):
-    pass
 
--- weather/main/views.py
+++ weather/main/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from api.models import Temperature
 
-# Create your views here.
+
 def home(request):
-    return render(request, 'home.html')
+    # Find the newest single temperature
+    temp =   Temperature.objects.order_by('recorded_time').first()
+    # Gather the total number of temperature readings gathered.
+    tcount = Temperature.objects.count()
+    # Find the first temperature entry recorded
+    tfirst = Temperature.objects.order_by('recorded_time').first()
+    
+    return render(request, 'home.html', {
+        'temp': temp,
+         'tcount': tcount, 
+         'tfirst': tfirst.recorded_time}
+    )
+
