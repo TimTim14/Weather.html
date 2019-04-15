22ae5ee5812db881801f23dc974c2956bb54c609
USER_NAME <EMAIL_ADDRESS>
2019-04-15 19:23:34 +0000

    r

--- weather/api/models.py
+++ weather/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Temperature(models.Model):
     celsius = models.FloatField(default=0.0)
     change = models.FloatField(default=0.0)
-    recorded_at = models.DateTimeField(auto_now=True)
+    recorder_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.celsius)
