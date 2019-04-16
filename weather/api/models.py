2bb00e5678e8885351a62e14b8e1c4c1c5c7456c
USER_NAME <EMAIL_ADDRESS>
2019-04-16 19:38:45 +0000

    j

--- weather/api/models.py
+++ weather/api/models.py
@@ -4,11 +4,24 @@ class Temperature(models.Model):
     celsius = models.FloatField(default=0.0)
     change = models.FloatField(default=0.0)
     recorded_at = models.DateTimeField(auto_now=True)
+    
 
-    def __str__(self):
+def __str__(self):
         return str(self.celsius)
-
+        
     @property
     def fahrenheit(self):
         "Returns the temperature in fahrenheit"
         return '%f' % ((self.celsius * 9/5) +32)
+        
+class Humidity(models.Model):
+    rh = models.FloatField(default=0.0)
+    change = models.FloatField(default=0.0)
+    recorded_at = models.DateTimeField(auto_now=True)
+
+def __str__(self):
+        return str(self.celsius)
+
+
+    
+
--- weather/api/views.py
+++ weather/api/views.py
@@ -4,9 +4,25 @@ from . import models, serializers
 # . import serializers TemperatureSerializer
 
 class TemperatureList(generics.ListCreateAPIView):
-    queryset = Temperature.objects.all()
-    serializer_class = TemperatureSerializer
+    queryset = models.Temperature.objects.all()
+    serializer_class = serializers.TemperatureSerializer
     
 class TemperatureDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Temperature.objects.all()
+    serializer_class = serializers.TemperatureSerializer
+
+class HumidityList(generics.ListCreateAPIView):
+    queryset = models.Humidity.objects.all()
+    serializers_class = serializers.HumiditySerializer
+    
+class HumidityList(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Humidity.objects.all()
+    serializer_class = serializers.TemperatureSerializer
+    
+class PressureList(generics.ListCreateAPIView):
+    queryset = models.Humidit.objects.all()
+    serializers_class = serializers.HumiditySerializer
+    
+class PressureList(generics.RetrieveUpdateDestroyAPIView):
     queryset = Temperature.objects.all()
     serializer_class = serializers.TemperatureSerializer
