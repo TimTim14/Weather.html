b15a437f4c6e4cce6de793edddd9666e38e098bb
USER_NAME <EMAIL_ADDRESS>
2019-04-16 19:09:45 +0000

    fix it

--- weather/main/views.py
+++ weather/main/views.py
@@ -4,15 +4,15 @@ from api.models import Temperature
 
 def home(request):
     # Find the newest single temperature
-    temp =   Temperature.objects.order_by('recorded_time').first()
+    temp =   Temperature.objects.order_by('recorded_at').first()
     # Gather the total number of temperature readings gathered.
     tcount = Temperature.objects.count()
     # Find the first temperature entry recorded
-    tfirst = Temperature.objects.order_by('recorded_time').first()
+    tfirst = Temperature.objects.order_by('recorded_at').first()
     
     return render(request, 'home.html', {
         'temp': temp,
          'tcount': tcount, 
-         'tfirst': tfirst.recorded_time}
+         'tfirst': tfirst.recorded_at}
     )
 
