8f985eb79cc818a2b81f5f5cef393ec48838ec96
USER_NAME <EMAIL_ADDRESS>
2019-04-17 19:28:30 +0000

    x

--- weather/api/urls.py
+++ weather/api/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.TemperatureList.as_view()),
     path('<int:pk>/', views.TemperatureDetail.as_view()),
-    path('', views.HumidityList.as_view()),
-    path('<int:pk>/', views.HumidityDetail.as_view()),
-    path('', views.PressureList.as_view()),
-    path('<int:pk>/', views.PressureDetail.as_view()),
+    path('rh', views.HumidityList.as_view()),
+    path('<rh/int:pk>/', views.HumidityDetail.as_view()),
+    path('bp', views.PressureList.as_view()),
+    path('<bp/int:pk>/', views.PressureDetail.as_view()),
     ]
     
