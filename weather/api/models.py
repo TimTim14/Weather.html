f48a5dd73857cb36a9e64d51f0f964532a5bdec6
USER_NAME <EMAIL_ADDRESS>
2019-04-30 19:14:24 +0000

    k

--- weather/api/models.py
+++ weather/api/models.py
@@ -14,7 +14,7 @@ class Temperature(models.Model):
     @property
     def fahrenheit(self):
         "Returns the temperature in fahrenheit"
-        return '%f' % ((self.celsius * 5/9) +32)
+        return '%f' % ((self.celsius * 9/5) +32)
             
 class Humidity(models.Model):
     rh = models.FloatField(default=0.0)
